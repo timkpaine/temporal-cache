6896d08670b444605a896f10dee21ac3d4ed2092
Tim Paine <t.paine154@gmail.com>
2018-12-29 21:34:32 -0500

    fixes #3, fixes #2

--- temporalcache/__init__.py
+++ temporalcache/__init__.py
@@ -1,5 +1,5 @@
-from .interval import interval
-from .expire import expire
-from .utils import TCException
+from .interval import interval  # noqa: F401
+from .expire import expire  # noqa: F401
+from .utils import TCException  # noqa: F401
 
 __version__ = '0.0.3'
