57735ece86342a8071ad978717c5d6087e7e6459
Tim Paine <t.paine154@gmail.com>
2019-12-08 10:44:32 -0500

    .

--- tests/test_persistent.py
+++ tests/test_persistent.py
@@ -1,47 +1,49 @@
+import os
 import datetime
 from mock import patch, MagicMock
 from tempfile import NamedTemporaryFile
 
 
-class TestExpire:
-    def setup(self):
-        self._olddatetime = datetime.datetime
+if os.name != "nt":
+    class TestExpire:
+        def setup(self):
+            self._olddatetime = datetime.datetime
 
-        class NewDateTime(datetime.datetime):
-            @classmethod
-            def now(cls):
-                ret = self._now + self._delay
-                print(self._now)
-                print(ret)
-                return ret
+            class NewDateTime(datetime.datetime):
+                @classmethod
+                def now(cls):
+                    ret = self._now + self._delay
+                    print(self._now)
+                    print(ret)
+                    return ret
 
-        datetime.datetime = NewDateTime
+            datetime.datetime = NewDateTime
 
-    def teardown(self):
-        datetime.datetime = self._olddatetime
+        def teardown(self):
+            datetime.datetime = self._olddatetime
 
-    def test_blank(self):
-        from random import random
-        from temporalcache import expire
+        def test_blank(self):
+            from random import random
+            from temporalcache import expire
 
-        self._now = datetime.datetime(2018, 1, 1, 1, 1, 0)
-        self._delay = datetime.timedelta(seconds=0)
+            self._now = datetime.datetime(2018, 1, 1, 1, 1, 0)
+            self._delay = datetime.timedelta(seconds=0)
 
-        with NamedTemporaryFile() as f:
-            @expire(1, persistent=f.name, maxsize=2)
-            def foo(test):
-                return random()
+            with NamedTemporaryFile() as f:
+                @expire(1, persistent=f.name, maxsize=2)
+                def foo(test):
+                    return random()
 
-            print('running first')
-            x = foo('a')
-            print('checking cached')
-            assert x == foo('a')
-            foo('b')
-            foo('c')
-            x = foo('d')
+                print('running first')
+                x = foo('a')
+                print('checking cached')
+                assert x == foo('a')
+                foo('b')
+                foo('c')
+                x = foo('d')
 
-            # expire
-            self._delay = datetime.timedelta(seconds=2)
-            print('checking cache expired')
-            assert x != foo('d')
-            print('success')
+                # expire
+                self._delay = datetime.timedelta(seconds=2)
+                print('checking cache expired')
+                assert x != foo('d')
+                print('success')
