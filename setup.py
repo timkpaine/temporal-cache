901024f49ac552c16a24d3b62deb136a7856c7cd
Tim Paine <t.paine154@gmail.com>
2021-02-27 16:53:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/timkpaine/{name}".format(name=name),
     author="Tim Paine",
-    author_email="timothy.k.paine@gmail.com",
+    author_email="t.paine154@gmail.com",
     license="Apache 2.0",
     install_requires=requires,
     extras_require={
